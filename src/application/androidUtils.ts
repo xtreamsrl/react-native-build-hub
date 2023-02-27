0c2a3c29e482926db52560e638ecbfbc9b3047be
Luca Micieli <luca.micieli@xtreamers.io>
2023-02-27 22:25:36 +0100

    feat: make flavor config optional for android

--- src/application/androidUtils.ts
+++ src/application/androidUtils.ts
@@ -1,7 +1,7 @@
 import {getAppName, getRootDestinationFolder} from './utils'
 
-export function getAppBuildFolder(buildType: string) {
+export function getAppBuildFolder(buildType?: string) {
   // todo add debug
-  const appPath = `${getRootDestinationFolder()}/android/${buildType}/debug`
+  const appPath = `${getRootDestinationFolder()}/android/${buildType?`${buildType}/`:''}debug`
   return appPath
 }

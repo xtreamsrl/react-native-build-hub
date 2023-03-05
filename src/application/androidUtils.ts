6c0584b98882c1752b5eeb9208fe78a7bcfcdeaf
Luca Micieli <luca.micieli@xtreamers.io>
2023-03-05 15:13:17 +0100

    style: fix formatting

--- src/application/androidUtils.ts
+++ src/application/androidUtils.ts
@@ -1,19 +1,19 @@
-import { getProjectRootDir, getRootDestinationFolder } from "./utils";
-import path from "path";
-import fs from "fs";
+import { getProjectRootDir, getRootDestinationFolder } from './utils';
+import path from 'path';
+import fs from 'fs';
 
 export function getAppBuildFolder(flavorName?: string, release?: boolean) {
-  const buildType = release ? "release" : "debug";
+  const buildType = release ? 'release' : 'debug';
 
-  const appPath = `${getRootDestinationFolder()}/android/${flavorName ? `${flavorName}/` : ""}${buildType}`;
+  const appPath = `${getRootDestinationFolder()}/android/${flavorName ? `${flavorName}/` : ''}${buildType}`;
   return appPath;
 }
 
 export function getAndroidIndexJsPath() {
-  const androidSpecific = path.join(getProjectRootDir(), "src", "application", "index.android.js");
+  const androidSpecific = path.join(getProjectRootDir(), 'src', 'application', 'index.android.js');
   if (fs.existsSync(androidSpecific)) {
     return androidSpecific;
   } else {
-    return path.join(getProjectRootDir(), "src", "application", "index.js");
+    return path.join(getProjectRootDir(), 'src', 'application', 'index.js');
   }
 }

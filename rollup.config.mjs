f44b70d5f266f76ecab7ef3159dce1f779317293
Luca Micieli <luca.micieli@xtreamers.io>
2023-08-21 14:09:49 +0200

    feat: add build with code obfuscation

---
+++ rollup.config.mjs
@@ -0,0 +1,84 @@
+import fs from "node:fs";
+import dotEnv from "dotenv";
+
+import commonjs from "@rollup/plugin-commonjs";
+import replace from "@rollup/plugin-replace";
+import JavaScriptObfuscator from "javascript-obfuscator";
+
+/**
+ *
+ * @param options {import("javascript-obfuscator").ObfuscatorOptions}
+ * @returns {{transform(*, *): {code: string}, name: string}|{code: string}}
+ */
+function javascriptObfuscator(options = {}) {
+  return {
+    name: "javascript-obfuscator",
+    transform(code, id) {
+      const obfuscationResult = JavaScriptObfuscator.obfuscate(code, options);
+      console.log("obfuscating...");
+      let result = { code: obfuscationResult.getObfuscatedCode() };
+
+      if (options.sourceMap && options.sourceMapMode !== "inline") {
+        result.map = obfuscationResult.getSourceMap();
+      }
+
+      return result;
+    }
+  };
+
+}
+
+const env = dotEnv.config().parsed;
+
+export default {
+  input: fs.readdirSync("./build/commands")
+    .filter(f => f.endsWith("js")).map(file => `./build/commands/${file}`)
+    .reduce((acc, file) => {
+      acc[file.replace("./build/", "").replace(".js", "")] = file;
+      return acc;
+    }, {
+      index: "./build/index.js"
+    }),
+  output: {
+    dir: "dist",
+    chunkFileNames: "includes/[hash].js",
+    format: "cjs"
+  },
+  plugins: [
+    replace(
+      Object.fromEntries(Object.entries(env).map(([k, v]) => [`process.env.${k}`, `"${v}"`]))
+    ),
+    commonjs(),
+    javascriptObfuscator({
+      compact: true,
+      controlFlowFlattening: true,
+      controlFlowFlatteningThreshold: 1,
+      deadCodeInjection: true,
+      deadCodeInjectionThreshold: 1,
+      debugProtection: true,
+      debugProtectionInterval: 4000,
+      disableConsoleOutput: true,
+      identifierNamesGenerator: 'hexadecimal',
+      log: false,
+      numbersToExpressions: true,
+      renameGlobals: false,
+      selfDefending: true,
+      simplify: true,
+      splitStrings: true,
+      splitStringsChunkLength: 5,
+      stringArray: true,
+      stringArrayCallsTransform: true,
+      stringArrayEncoding: ['rc4'],
+      stringArrayIndexShift: true,
+      stringArrayRotate: true,
+      stringArrayShuffle: true,
+      stringArrayWrappersCount: 5,
+      stringArrayWrappersChainedCalls: true,
+      stringArrayWrappersParametersMaxCount: 5,
+      stringArrayWrappersType: 'function',
+      stringArrayThreshold: 1,
+      transformObjectKeys: true,
+      unicodeEscapeSequence: false
+    })]
+
+};
--- src/commands/addUserToProject.ts
+++ src/commands/addUserToProject.ts
@@ -14,20 +14,6 @@ export default class AddUserToProject extends AuthenticatedCommand {
   public async run(): Promise<void> {
     const { args, flags } = await this.parse(AddUserToProject);
 
-    const projects = await getAvailableProjects();
-    const selectedProject = await prompts({
-      type: 'select',
-      name: 'project',
-      message: 'Select the project you want to use',
-      choices: projects.map(p => ({
-        title: `${chalk.bold(p.name)} (${p.id})`,
-        value: p,
-      })),
-    });
-    if (selectedProject.project) {
-      logger.info(`Selected project ${chalk.bold(selectedProject.project.name)} (${selectedProject.project.id})`);
-      await saveProjectData(selectedProject.project.name, selectedProject.project.id);
-      this.exit(0);
-    }
+    // todo
   }
 }
